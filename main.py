b898863331c25a5ae686334ba4b956f51e2da3d3
No-name777 <lhj2198481@naver.com>
2025-06-14 00:38:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import time
 from pymongo import MongoClient
+
 from binance_ws import listen_binance_liquidations
+from bybit_ws import listen_bybit_liquidations
+from okx_ws import listen_okx_liquidations
+from htx_ws import listen_htx_liquidations
+from deribit_ws import listen_deribit_liquidations
 
 from config import MONGO_URI
 
@@ -9,4 +14,12 @@ db = client["liquidation_data"]
 
 if __name__ == "__main__":
     print("📡 시작: 실시간 청산 수집 중...")
+
     listen_binance_liquidations(db)
+    listen_bybit_liquidations(db)
+    listen_okx_liquidations(db)
+    listen_htx_liquidations(db)
+    listen_deribit_liquidations(db)
+
+    while True:
+        time.sleep(1)
